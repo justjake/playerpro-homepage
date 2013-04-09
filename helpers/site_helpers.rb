cb310201e333cab21b146d51bd9b7defe88ef59b
Jake Teton-Landis <just.1.jake@gmail.com>
2013-04-09 15:25:13 -0700

    spiking the demo page style real fast-like

--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,7 +1,7 @@
 module SiteHelpers
 
   def page_title
-    title = "Set your site title in /helpers/site_helpers.rb"
+    title = "PlayerPro"
     if data.page.title
       title << " | " + data.page.title
     end
@@ -12,9 +12,17 @@ module SiteHelpers
     if data.page.description
       description = data.page.description
     else
-      description = "Set your site description in /helpers/site_helpers.rb"
+      description = "Sports managmenet from the future"
     end
     description
+  end
+
+  # Definition list items
+  def define(key, value)
+      """
+      <dt>#{key}:</dt>
+      <dd>#{value}</dd>
+      """
   end
 
 end
